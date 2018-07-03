8562e415a25af891d970996afeb2ab25ff6439d0
alwaysrunning <178224406@qq.com>
2018-07-03 11:12:08 +0800

    mysql

--- mysql/connect.js
+++ mysql/connect.js
@@ -1,8 +1,11 @@
 var mysql = require("mysql")
 var fs = require("fs")
 var path = require("path")
-var dbData, sql;
-
-var content = fs.readFileSync(path.join(__dirname,'./mysql.json'))
+var dbData, sql, content;
+if(process.env.NODE_ENV === 'production'){
+    content = fs.readFileSync(path.join(__dirname,'./mysql_pro.json'))
+}else{
+    content = fs.readFileSync(path.join(__dirname,'./mysql.json'))
+}
 sql = mysql.createConnection(JSON.parse(content.toString()))
 module.exports = sql
