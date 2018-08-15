a7af52379c6bb32ae7bd2683c269ffcfb10ae076
yanghaitao <178224406@qq.com>
2018-08-15 09:53:30 +0800

    node_env

--- mysql/connect.js
+++ mysql/connect.js
@@ -3,7 +3,12 @@ var fs = require("fs")
 var path = require("path")
 var dbData, sql, content;
 
-content = fs.readFileSync(path.join(__dirname,'./mysql_pro.json'))
-//content = fs.readFileSync(path.join(__dirname,'./mysql.json'))
+var env = process.env.NODE_ENV || 'production'
+if(env=="development"){
+    content = fs.readFileSync(path.join(__dirname,'./mysql.json'))
+}else{
+    content = fs.readFileSync(path.join(__dirname,'./mysql_pro.json'))
+}
+
 sql = mysql.createConnection(JSON.parse(content.toString()))
 module.exports = sql
